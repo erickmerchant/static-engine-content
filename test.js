7af5400b7540b96c04a7ce7610dfd7127deb3750
Erick Merchant <erickmerchant@gmail.com>
2015-04-12 00:08:58 -0700

    Move test files

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 var describe = require('mocha').describe
 var it = require('mocha').it
 var assert = require('assert')
-var content = require('./rewired')({
+var plugin = mock({
   './test/content/': {
     'test.txt': 'test 2'
   }
 })
-var plugin = content('./test/content/*')
+var content = plugin('./test/content/*')
 
 describe('plugin', function () {
   it('it should append to existing pages', function (done) {
-    plugin([{content: 'test 1'}], function (err, pages) {
+    content([{content: 'test 1'}], function (err, pages) {
       if (err) {
         done(err)
       } else {
@@ -21,3 +21,19 @@ describe('plugin', function () {
     })
   })
 })
+
+function mock (fsConfig) {
+  var rewire = require('rewire')
+  var mockFS = require('mock-fs')
+  var plugin = rewire('./index.js')
+  var glob = rewire('glob')
+  var fs = mockFS.fs(fsConfig)
+
+  glob.__set__('fs', fs)
+
+  plugin.__set__('glob', glob)
+
+  plugin.__set__('fs', fs)
+
+  return plugin
+}
