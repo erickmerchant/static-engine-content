14d56ca6d635abd1d7fe25f6f7ee68d2326fd03f
Erick Merchant <erickmerchant@gmail.com>
2015-04-17 21:15:33 -0700

    Add test coverage

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ module.exports = function (content) {
 
           done(null, pages)
         })
+        .catch(function (err) {
+          done(err)
+        })
       }
     })
   }
--- test.js
+++ test.js
@@ -1,36 +1,72 @@
 var describe = require('mocha').describe
 var it = require('mocha').it
 var assert = require('assert')
-var plugin = mock({
-  './test/content/test.txt': 'test 2'
-})
-var content = plugin('./test/content/*')
 
 describe('plugin', function () {
   it('it should append to existing pages', function (done) {
-    content([{content: 'test 1'}], function (err, pages) {
-      if (err) {
-        done(err)
-      } else {
-        assert.deepEqual(pages, [{content: 'test 1'}, {content: 'test 2', file: './test/content/test.txt'}])
+    var plugin = mock({
+      './test/content/test.txt': 'test 2'
+    })
+    var content = plugin('./test/content/*')
 
-        done()
-      }
+    content([{content: 'test 1'}], function (err, pages) {
+      assert.equal(null, err)
+      assert.deepEqual(pages, [{content: 'test 1'}, {content: 'test 2', file: './test/content/test.txt'}])
+
+      done()
+    })
+  })
+
+  it('it should handle glob errors', function (done) {
+    var plugin = mock({
+      './test/content/test.txt': 'test 2'
+    }, {
+      glob: new Error('glob error!')
+    })
+    var content = plugin('./test/content/*')
+
+    content([{content: 'test 1'}], function (err) {
+      assert.equal(err.message, 'glob error!')
+
+      done()
+    })
+  })
+
+  it('it should handle fs.readFile errors', function (done) {
+    var plugin = mock({
+      './test/content/test.txt': 'test 2'
+    }, {
+      fs: new Error('fs.readFile error!')
+    })
+    var content = plugin('./test/content/*')
+
+    content([{content: 'test 1'}], function (err) {
+      assert.equal(err.message, 'fs.readFile error!')
+
+      done()
     })
   })
 })
 
-function mock (files) {
+function mock (files, errors) {
   var rewire = require('rewire')
   var plugin = rewire('./index.js')
 
   plugin.__set__('glob', function (pattern, options, callback) {
-    callback(null, Object.keys(files))
+    if (errors && errors.glob) {
+      callback(errors.glob)
+    } else {
+      callback(null, Object.keys(files))
+    }
   })
 
   plugin.__set__('fs', {
     readFile: function (filename, options, callback) {
-      callback(null, files[filename])
+      if (errors && errors.fs) {
+        callback(errors.fs)
+      } else {
+        callback(null, files[filename])
+      }
     }
   })
 
